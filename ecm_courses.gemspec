548bd90acd0b3ff2c2f1f138dc0705d01488925f
Roberto Vasquez Angel <robotex@robotex.de>
2012-07-02 12:20:45 +0200

    Working active admin test example.

--- ecm_courses.gemspec
+++ ecm_courses.gemspec
@@ -33,6 +33,7 @@ Gem::Specification.new do |s|
   
   # # Tests
   s.add_development_dependency "rspec-rails", "~> 2.0"
+  s.add_development_dependency "factory_girl_rails", "~> 3.0"
   # s.add_development_dependency "capybara"
   # s.add_development_dependency "guard-rspec"
   # s.add_development_dependency "guard-spork"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,10 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+# Factory girl
+require 'factory_girl'
+FactoryGirl.find_definitions
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
@@ -18,7 +22,7 @@ RSpec.configure do |config|
   # config.mock_with :rr
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
