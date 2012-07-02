2a397b79f52e6016ce85a0b94fcc9b0f65e62395
Roberto Vasquez Angel <robotex@robotex.de>
2012-07-02 10:00:32 +0200

    Configured active admin.

--- lib/ecm_courses.rb
+++ lib/ecm_courses.rb
@@ -1,2 +1,9 @@
+require 'rubygems'
+
+# Ecm::Admin should do this!
+require 'activeadmin' 
+require 'sass-rails'
+require 'meta_search'
+
 require 'ecm/courses/engine'
 require 'ecm/courses/version'
