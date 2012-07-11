a01bf44a2d4d3aec56843e35df9be272c20c29b9
Roberto Vasquez Angel <robotex@robotex.de>
2012-07-11 12:19:20 +0200

    syck yaml parsing.

--- spec/dummy/config/boot.rb
+++ spec/dummy/config/boot.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 gemfile = File.expand_path('../../../../Gemfile', __FILE__)
 
+# Switch yaml engine
+YAML::ENGINE.yamler= 'syck' if defined?(YAML::ENGINE)
+
 if File.exist?(gemfile)
   ENV['BUNDLE_GEMFILE'] = gemfile
   require 'bundler'
